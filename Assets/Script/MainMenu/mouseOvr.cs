f1f649598f989660b53ea2fac199b534188e0b7c
pernandoang <102746832+pernandoang@users.noreply.github.com>
2024-06-11 18:26:09 +0700

    main
    rect transform

--- Assets/Script/MainMenu/mouseOvr.cs
+++ Assets/Script/MainMenu/mouseOvr.cs
@@ -5,20 +5,21 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class mouseOvr : MonoBehaviour, IPointerEnterHandler
+public class mouseOvr : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
 {
-    public Vector2 posisi;
-    public RectTransform rect1;
-
-    public void ChangePosition(Vector2 newPosition)
-    {
-        rect1.anchoredPosition = newPosition;
-    }
+    public Vector2[] StartPosition;
+    public Vector2[] posisi;
+    public RectTransform[] rect;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        ChangePosition(posisi);
+        rect[0].anchoredPosition = posisi[0];
+        rect[1].anchoredPosition = posisi[1];
     }
 
-    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rect[0].anchoredPosition = StartPosition[0];
+        rect[1].anchoredPosition = StartPosition[1];
+    }
 }
