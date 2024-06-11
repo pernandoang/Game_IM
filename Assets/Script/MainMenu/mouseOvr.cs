8b1c9f8746c9f85cea731aa05565d2c3815888fd
pernandoang <102746832+pernandoang@users.noreply.github.com>
2024-06-11 18:43:12 +0700

    update

--- Assets/Script/MainMenu/mouseOvr.cs
+++ Assets/Script/MainMenu/mouseOvr.cs
@@ -7,21 +7,21 @@ using UnityEngine.EventSystems;
 
 public class mouseOvr : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
 {
-    public GameObject obj;
+    public GameObject[] obj;
     public Vector2[] StartPosition;
     public Vector2[] posisi;
     public RectTransform[] rect;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        obj.SetActive(true);
+        obj[0].SetActive(true);
         rect[0].anchoredPosition = posisi[0];
         rect[1].anchoredPosition = posisi[1];
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        obj.SetActive(false);
+        obj[0].SetActive(false);
         rect[0].anchoredPosition = StartPosition[0];
         rect[1].anchoredPosition = StartPosition[1];
     }
