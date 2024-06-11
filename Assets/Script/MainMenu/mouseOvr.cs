f9bb4dc5067f6df2eca5e7a29529a57f26c5497e
pernandoang <102746832+pernandoang@users.noreply.github.com>
2024-06-11 18:12:03 +0700

    Update mouseOvr.cs
    apple pindah

--- Assets/Script/MainMenu/mouseOvr.cs
+++ Assets/Script/MainMenu/mouseOvr.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class mouseOvr : MonoBehaviour, IPointerEnterHandler
 {
-    public GameObject[] obj;
-    public Vector3 current1;
-    public Vector3 current2;
+    public Vector2 posisi;
+    public RectTransform rect1;
+
+    public void ChangePosition(Vector2 newPosition)
+    {
+        rect1.anchoredPosition = newPosition;
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        ChangePosition(posisi);
     }
+
+    
 }
